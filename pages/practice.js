7ccdf85acd7a2d07130417eb566226da14a0456c
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-06-03 20:30:43 +0200

    feat (app): adding a botton to back page home

--- pages/practice.js
+++ pages/practice.js
@@ -3,6 +3,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import ButtonOnclick from '../components/button'
 import { Button, Container, Form, Row } from 'react-bootstrap'
+import Link from 'next/link'
 
 const Practice = () => {
   const [word, setWord] = useState([])
@@ -84,11 +85,13 @@ const Practice = () => {
   }
   return (
     <main className='vh-100'>
-      <div clasName='m-4'>
-        <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-arrow-left-square' viewBox='0 0 16 16'>
-          <path fill-rule='evenodd' d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z' />
-        </svg>
-      </div>
+      <Button variant='outline-secondary' className='m-4'>
+        <Link href='/'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-arrow-left' viewBox='0 0 16 16'>
+            <path fill-rule='evenodd' d='M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z' />
+          </svg>
+        </Link>
+      </Button>
       <Container className='h-50 d-flex justify-content-center align-items-center'>
         <Row>
 

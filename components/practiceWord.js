e74b08213da8bf38113086e4c3b8089989fefe3a
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-08-16 16:12:10 +0200

    style (PracticeWord): remove space m and move Overlay up h1

--- components/practiceWord.js
+++ components/practiceWord.js
@@ -4,23 +4,20 @@ import { Button, Form, Tooltip, Overlay } from 'react-bootstrap'
 import { useWordPractice } from '../hooks/useWordPractice'
 
 export const PracticeWord = () => {
-  const { word, showWord, wordSpanish, message, target, showTooltip, newStateWord, setWord, handleOnchage, handleCheck, haldleNext, handleshowTooltip, setWordSpanish, setMessage, setNewStateWord, setShowWord } = useWordPractice()
+  const { word, showWord, wordSpanish, message, target, showTooltip, newStateWord, setWord, handleOnchage, handleCheck, haldleNext, handleshowTooltip, setWordSpanish, setMessage, setNewStateWord, setShowWord, setshowTooltip } = useWordPractice()
   return (
     <>
-      <h1 className='display-1 text-center mb-3' ref={target} onClick={() => handleshowTooltip()}>
-        {word[showWord] ? word[showWord].data.english : ''}
-      </h1>
       <Overlay target={target.current} show={showTooltip} placement='top'>
         <Tooltip id='tooltip-top'>
           <strong>{word[showWord] ? word[showWord].data.spanish : ''}</strong>
         </Tooltip>
-
       </Overlay>
+      <h1 className='display-1 text-center mb-3' ref={target} onClick={() => handleshowTooltip(showTooltip, setshowTooltip)}>
+        {word[showWord] ? word[showWord].data.english : ''}
+      </h1>
       <Form>
         <Form.Control ref={target} className='form-floating' size='lg' placeholder='Escriba en Español' onChange={(e) => handleOnchage(e, setWordSpanish)} type='text' value={wordSpanish} />
-
         <section className='fixed-bottom h-25 bg-light d-flex justify-content-center align-items-center'>
-
           {message === ''
             ? <ButtonOnclick disabled handleClick={() => handleCheck(wordSpanish, word, showWord, setMessage, setNewStateWord, setShowWord)}>comprobar</ButtonOnclick>
             : (<div className='d-flex flex-column justify-items-center align-items-center'>

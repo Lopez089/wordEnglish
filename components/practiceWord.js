d157da1af2523e54f55f8ae9c848c9eb375cac2d
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-08-16 15:43:14 +0200

    refactor (pranciceWord): extract the compnent  practiceWord to page practice

---
+++ components/practiceWord.js
@@ -0,0 +1,35 @@
+
+import ButtonOnclick from '../components/button'
+import { Button, Form, Tooltip, Overlay } from 'react-bootstrap'
+import { useWordPractice } from '../hooks/useWordPractice'
+
+export const PracticeWord = () => {
+  const { word, showWord, wordSpanish, message, target, showTooltip, handleOnchage, handleCheck, haldleNext, handleshowTooltip, setWordSpanish, setMessage, setNewStateWord, setShowWord } = useWordPractice()
+  return (
+    <>
+      <h1 className='display-1 text-center mb-3' ref={target} onClick={() => handleshowTooltip()}>
+        {word[showWord] ? word[showWord].data.english : ''}
+      </h1>
+      <Overlay target={target.current} show={showTooltip} placement='top'>
+        <Tooltip id='tooltip-top'>
+          <strong>{word[showWord] ? word[showWord].data.spanish : ''}</strong>
+        </Tooltip>
+
+      </Overlay>
+      <Form>
+        <Form.Control ref={target} className='form-floating' size='lg' placeholder='Escriba en Español' onChange={(e) => handleOnchage(e, setWordSpanish)} type='text' value={wordSpanish} />
+
+        <section className='fixed-bottom h-25 bg-light d-flex justify-content-center align-items-center'>
+
+          {message === ''
+            ? <ButtonOnclick disabled handleClick={() => handleCheck(wordSpanish, word, showWord, setMessage, setNewStateWord, setShowWord)}>comprobar</ButtonOnclick>
+            : (<div className='d-flex flex-column justify-items-center align-items-center'>
+              <p className='fs-5 lead'>{message}</p>
+              <Button onClick={haldleNext}>siguiente</Button>
+            </div>)}
+
+        </section>
+      </Form>
+    </>
+  )
+}

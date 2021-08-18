e9942810cd8ee18b41099af51ca4ab722c63e1a5
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-08-18 18:51:40 +0200

    feat (useWordPractice): useEffect carculate to progress bar

--- hooks/useWordPractice.js
+++ hooks/useWordPractice.js
@@ -1,10 +1,12 @@
 import { useEffect, useRef, useState } from 'react'
-import { handleOnchage, handleCheck, haldleNext, handleshowTooltip } from '../util/util'
+import { handleOnchage, handleCheck, haldleNext, handleshowTooltip, calculateProgress } from '../util/util'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 export const useWordPractice = () => {
   const [word, setWord] = useState([])
+  const [totalNumberWord, setTotalNumberWord] = useState(0)
+  const [progressBar, setProgressBar] = useState(0)
   const [newStateWord, setNewStateWord] = useState([])
   const [showWord, setShowWord] = useState(0)
   const [wordSpanish, setWordSpanish] = useState('')
@@ -22,11 +24,16 @@ export const useWordPractice = () => {
           }
 
           setWord((preState) => preState.concat(newState))
+          setTotalNumberWord(querySnapshot.size)
         })
       )
   }, [])
 
+  useEffect(() => {
+    setProgressBar(calculateProgress(totalNumberWord, word))
+  }, [word])
+
   return {
-    word, showWord, wordSpanish, message, target, showTooltip, handleOnchage, handleCheck, haldleNext, newStateWord, handleshowTooltip, setWordSpanish, setMessage, setNewStateWord, setShowWord, setWord, setshowTooltip
+    progressBar, word, showWord, wordSpanish, message, target, showTooltip, handleOnchage, handleCheck, haldleNext, newStateWord, handleshowTooltip, setWordSpanish, setMessage, setNewStateWord, setShowWord, setWord, setshowTooltip
   }
 }

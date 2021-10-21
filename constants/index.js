971ad0f1399547faae172132fe7d69da18f1676d
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-10-21 15:19:56 +0200

    feat: create contantes modalConfimation

---
+++ constants/index.js
@@ -0,0 +1,13 @@
+export const modalConfimation = {
+  delete: {
+    header: '¿Deseas eliminar el items?'
+  },
+  edit: {
+    header: '¿Deseas editar el items?'
+  },
+  button: {
+    yes: 'SI',
+    no: 'NO'
+  }
+
+}
